62ef933bbdc402b5fd041a6d20c1d70614816386
Aniket <aniketupatel1@gmail.com>
2019-10-19 22:32:02 -0400

    added slide and fade transitions to highlight discounts and description

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Header from './Components/header_footer/Header'
 
 import Featured from './Components/featured'
 import VenueNfo from './Components/venueNfo'
+import Highlights from './Components/highlights'
 
 class App extends React.Component {
   
 
   render() {
     return (
-    <div className="App" style={{height: "1900px", background:'red'}}>
+    <div className="App" style={{height: "1500px", background:'red'}}>
       <Header/>
       <Featured/>
       <VenueNfo/>
+      <Highlights/>
     </div>
     )
   }
